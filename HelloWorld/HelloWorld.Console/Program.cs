c911aa794737425a8bc2b57d1ba518d7aa2e1716
blvining <brittanyy2@gmail.com>
2018-08-18 20:45:12 -0400

    Completed HelloWorld

---
+++ HelloWorld/HelloWorld.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld.Console
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
